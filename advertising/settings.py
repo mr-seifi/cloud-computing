b12e7eb14e43057a6eab35b8033752ac4ab35ec0
super amin <aminseiifi@gmail.com>
2022-11-18 16:03:50 +0330

    fix(config): add host ip to allowed hosts

--- advertising/settings.py
+++ advertising/settings.py
@@ -27,7 +27,10 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    os.getenv('HOST_IP')
+]
+
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
